f971ad4a2d227be0f68fd03aaf95d66da3f12441
PhucKhanh <nthtpk7h7i@gmail.com>
2024-01-13 22:17:32 +0700

    Update

--- js/page-message/eventListenerListUserInbox.js
+++ js/page-message/eventListenerListUserInbox.js
@@ -6,8 +6,8 @@ list_inbox__content__ul__li.forEach((item) => {
 
     let inbox_has_been_no_read = item.querySelector('.list_inbox__content__ul__li-inbox_has_been_no_read');
     let icon_notification_new_message = item.querySelector('.list_inbox__content__ul__li-icon_notification_new_message');
-    if (!inbox_has_been_no_read) {
-        icon_notification_new_message.style.display = 'none';
+    if (inbox_has_been_no_read) {
+        icon_notification_new_message.classList.add('active');
     }
     item.addEventListener('click', function() {
         list_inbox__content__ul__li.forEach((childItem) => {
@@ -19,10 +19,10 @@ list_inbox__content__ul__li.forEach((item) => {
         if (inbox_has_been_no_read) {
             inbox_has_been_no_read.classList.remove('list_inbox__content__ul__li-inbox_has_been_no_read');
             inbox_has_been_no_read.classList.add('list_inbox__content__ul__li-inbox_has_been_read');
-            icon_notification_new_message.style.display = 'none';
+            icon_notification_new_message.classList.remove('active');
         }
         item.classList.add('active');
         not_select_inbox.style.display = 'none';
-        detail_inbox.style.display = 'flex';
+        detail_inbox.classList.add('active');
     });
 });
